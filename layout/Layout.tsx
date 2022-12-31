7215fe7ff46755565b9461d2598846efc0afddfe
SerVik888 <genom.86@inbox.ru>
2022-12-31 18:41:48 +0300

    add Layout

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -4,26 +4,30 @@ import { Header } from "./Header/Header";
 import { Sidebar } from "./Sidebar/Sidebar";
 import { Footer } from "./Footer/Footer";
 import { FunctionComponent } from "react";
+import { AppContextProvider, IAppContext } from "../context/app.context";
 
 const Layout = ({ children }: LayoutProps): JSX.Element => {
 	return (
-		<>
-			<Header />
-			<div>
-				<Sidebar />
-				{children}
-			</div>
-			<Footer />
-		</>
+		<div className={s.wrapper}>
+			<Header className={s.header} />
+			<Sidebar className={s.sidebar} />
+			<div className={s.body}> {children}</div>
+
+			<Footer className={s.footer} />
+		</div>
 	);
 };
 
-export const withLayout = <T extends Record<string, unknown>>(Component: FunctionComponent<T>) => {
+export const withLayout = <T extends Record<string, unknown> & IAppContext>(
+	Component: FunctionComponent<T>
+) => {
 	return function withLayoutComponent(props: T): JSX.Element {
 		return (
-			<Layout>
-				<Component {...props} />
-			</Layout>
+			<AppContextProvider menu={props.menu} firstCategory={props.firstCategory}>
+				<Layout>
+					<Component {...props} />
+				</Layout>
+			</AppContextProvider>
 		);
 	};
 };
--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -1,6 +1,15 @@
 import { SidebarProps } from "./SidebarProps";
-import s from "./Layout.module.css";
+import s from "./Sidebar.module.css";
+import cn from "classnames";
+import { Menu } from "../Menu/Menu";
+import Logo from "../logo.svg";
 
-export const Sidebar = ({ ...props }: SidebarProps): JSX.Element => {
-	return <div {...props}>SideBar</div>;
+export const Sidebar = ({ className, ...props }: SidebarProps): JSX.Element => {
+	return (
+		<div className={cn(className, s.sidebar)} {...props}>
+			<Logo className={s.logo} />
+			<div>поиск</div>
+			<Menu />
+		</div>
+	);
 };
